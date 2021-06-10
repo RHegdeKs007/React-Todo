e4da7e7e9a2c8e82401476ccb4a1e0c6fa11b362
Rakshith hegde <rakshithhgd@gmail.com>
2021-06-10 08:53:38 +0530

    deployed

--- src/Todo.js
+++ src/Todo.js
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Todo.css';
-import {Avatar, List, ListItem,ListItemAvatar,ListItemText,ImageIcon, Button } from '@material-ui/core';
+import {Avatar, List, ListItem,ListItemAvatar,ListItemText,ImageIcon, Button, Modal } from '@material-ui/core';
 import db from './firebase';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import { makeStyles } from '@material-ui/core/styles';
+import EditIcon from '@material-ui/icons/Edit';
+
+
+
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
 
 
 function Todo(props) {
+        const classes = useStyles();
+        const [open, setOpen] = useState(false);
+        const [input, setInput] = useState('');
+
+        const handleOpen = () => {
+            setOpen(true);
+        };
+        const updateTodo = () => {
+            db.collection('todos').doc(props.todo.id).set({
+                todo: input
+            },{merge : true});
+
+            setOpen(false);
+        }
     return (
+        <>
+        <Modal
+            open={open}
+            onclose={e=> setOpen(false)}
+        >
+            <div className = {classes.paper}>
+                <h1>I am a modal</h1>
+                <input placeholder={props.todo.todo} value ={input} onChange={event => setInput(event.target.value)} />
+                <Button onClick = {updateTodo}>Update todo</Button>
+            </div>
+            </Modal>
+
         <List className='todo_list'>
             <ListItem>
                 <ListItemAvatar>
@@ -14,8 +56,10 @@ function Todo(props) {
                 </ListItemAvatar>
                 <ListItemText primary={props.todo.todo} secondary="complete it today😄"  />
             </ListItem>
+            <EditIcon onClick = {e => setOpen(true)}></EditIcon>
             <DeleteForeverIcon onClick={event =>{db.collection('todos').doc(props.todo.id).delete()}} />
         </List>
+      </>  
     )
 }
 
